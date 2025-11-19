05cdd6d1d91d4c2d65580e13432c3fd33d9243af
Damayo,Edison Philip D. <edisondamayo@gmail.com>
2025-11-19 21:11:59 +0800

    second commit

--- src/main/java/com/damayo/lab_7/Product.java
+++ src/main/java/com/damayo/lab_7/Product.java
@@ -1,4 +1,14 @@
 package com.damayo.lab_7;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Product {
+    private Long id;
+    private String name;
+    private Double price;
 }
